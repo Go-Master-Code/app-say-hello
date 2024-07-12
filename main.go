8934e5c209ddadd7841ea3ac32c12a31107aee79
unknown <kevinkristansa@gmail.com>
2024-07-12 18:52:05 +0700

    Apps

--- main.go
+++ main.go
@@ -12,5 +12,6 @@ func main() {
 	belajar.LogInfo("Ini adalah method dari module belajar")
 	repos.LogInfo("Berhasil import method dari module repos")
 	//fmt.Println(repos.SayHello()) //method v1.5.0
-	fmt.Println(repos.SayHello("Hasil major changes")) //method tambahan dari versi modul v2.0.0
+	fmt.Println(repos.SayHello("Hasil major changes"))    //method tambahan dari versi modul v2.0.0
+	fmt.Println(repos.SayHello("Hasil major changes 2x")) //method tambahan dari versi modul v2.0.0
 }
