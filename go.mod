f8554175eed6801764e44a8bdc0c21393d4c0375
unknown <kevinkristansa@gmail.com>
2024-07-12 18:34:27 +0700

    Aplikasi

---
+++ go.mod
@@ -0,0 +1,11 @@
+module github.com/Go-Master-Code/app-say-hello
+
+go 1.22.4
+
+require github.com/Go-Master-Code/belajar v0.0.0-20240709095252-3263ee1def8e
+
+require github.com/Go-Master-Code/repos/v2 v2.0.0 // indirect
+
+//require github.com/Go-Master-Code/repos v1.5.0
+//require github.com/Go-Master-Code/repos/v2 v2.0.0 //nama module setelah major changes
+//ganti nama module baru secara manual dulu, coba go get lagi module nya sampai muncul
---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+
+	belajar "github.com/Go-Master-Code/belajar"
+	//repos "github.com/Go-Master-Code/repos"
+	repos "github.com/Go-Master-Code/repos/v2" //module dengan major changes
+)
+
+func main() {
+	belajar.LogInfo("Ini adalah method dari module belajar")
+	repos.LogInfo("Berhasil import method dari module repos")
+	//fmt.Println(repos.SayHello()) //method v1.5.0
+	fmt.Println(repos.SayHello("Hasil major changes")) //method tambahan dari versi modul v2.0.0
+}
